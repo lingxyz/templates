e27e6a1442ef06c03acbbb145885523cb0f934b6
Ling Z <771152260@qq.com>
2023-04-24 17:36:34 +0800

    feat: 动态生成路由

--- vue3-template/src/routes/index.ts
+++ vue3-template/src/routes/index.ts
@@ -1,18 +1,21 @@
-import { createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
+import { createRouter, createWebHistory} from "vue-router";
 
-const home = () => import('../pages/home.vue')
+// 获取pages下的所有页面
+const pages = import.meta.glob('../pages/**/*.vue')
+const pagePaths = Object.keys(pages)
 
-const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    redirect: '/home'
-  },
-  {
-    path: "/home",
-    name: "home",
-    component: home
-  },
-]
+// 循环pagePaths，将每个元素中的字符串“.”替换为“@/pages”
+const routes = pagePaths.map(path => {
+  // @ts-ignore
+  const name = path.match(/\.\/pages\/(.*)\.vue$/)[1].toLowerCase()
+  const component = () => import(`${path}`)
+
+  return {
+    path: `/${name ?? 'home'}`,
+    name: name, // 将页面名称添加到路由名称中
+    component // 动态导入页面组件
+  }
+})
 
 export const router = createRouter({
   history: createWebHistory(),
